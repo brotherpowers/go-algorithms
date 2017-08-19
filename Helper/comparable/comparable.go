4c0b506b15ddb3c8152cad3aa2da8d5e55c41d3d
Abhimanyu Sharma <abhimanyusharma003@gmail.com>
2017-08-19 13:03:53 +0530

    CS fixes added more test cases and minor tweaks

--- Helper/comparable/comparable.go
+++ Helper/comparable/comparable.go
@@ -1,7 +1,7 @@
 package comparable
 
 // Comparable interface
-type Comparable interface{
+type Comparable interface {
 	// -1 Lesser | 1 Greater | 0 Equals
 	Compare(Comparable) int
 }
--- stack/stack.go
+++ stack/stack.go
@@ -1,18 +1,17 @@
 package stack
 
-
-type Stack struct{
+type Stack struct {
 	Array []interface{}
 }
 
-func (s Stack) IsEmpty() bool{
+func (s Stack) IsEmpty() bool {
 	return len(s.Array) == 0
 }
 
-func (s Stack) Count() int{
+func (s Stack) Count() int {
 	return len(s.Array)
 }
 
-func (s *Stack) Push(element interface{}){
+func (s *Stack) Push(element interface{}) {
 	s.Array = append(s.Array, element)
 }
