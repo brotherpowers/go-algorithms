3c361239f92bd2de75aaf75b84e521a5a584e57f
harshvishu <mhacnagbani@gmail.com>
2017-08-26 16:44:10 +0000

    Added Shell sort

---
+++ Sort/shell/Examples/shellSort.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-algorithms/Sort/insertion"
+)
+
+func main() {
+
+	numbers := []int{11, 59, 3, 2, 53, 17, 31, 7, 19, 67, 47, 13, 37, 61, 29, 43, 5, 41, 23}
+
+	fmt.Println("Array before sorting", numbers)
+
+	// Performing insertion sort
+	insertion.Sort(numbers)
+
+	fmt.Println("Array after sorting", numbers)
+}
---
+++ Sort/shell/shell.go
@@ -0,0 +1,21 @@
+package shell
+
+func Sort(arr []int) {
+	length := len(arr)
+
+	if length == 0 {
+		panic("Array size acn not be zero")
+	}
+
+	if length == 1 {
+		return
+	}
+
+	for subListCount := length / 2; subListCount > 0; subListCount /= 2 {
+		for i := subListCount; i < length; i++ {
+			for j := i; j >= subListCount && arr[j-subListCount] > arr[j]; j -= subListCount {
+				arr[j], arr[j-subListCount] = arr[j-subListCount], arr[j]
+			}
+		}
+	}
+}
