a3479f704923a582fc36d1c8e49855be98744777
harshvishu <mhacnagbani@gmail.com>
2017-09-13 11:17:51 +0000

    Update: Kth largest Element

--- Search/kthLargest/kthLargest_test.go
+++ Search/kthLargest/kthLargest_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestNthLargest(t *testing.T) {
+func TestKthLargest(t *testing.T) {
 	cases := []struct {
 		in           []int
 		search, want int
@@ -16,6 +16,26 @@ func TestNthLargest(t *testing.T) {
 
 	for _, c := range cases {
 		got := KthLargest(c.in, c.search)
+
+		if got != c.want {
+			t.Errorf("%vth largest number in %v == %v, want = %v", c.search, c.in, got, c.want)
+		}
+	}
+}
+
+// Test KthLargestUsingHeap
+func TestKthLargestUsingHeap(t *testing.T) {
+	cases := []struct {
+		in           []int
+		search, want int
+	}{
+		{[]int{11, 59, 3, 2, 53, 17, 31, 7, 19, 67, 47, 13, 37, 61, 29, 43, 5, 41, 23}, 2, 59},
+		{[]int{5, 2, 4, 7}, 1, 5},
+		{[]int{0, 1, 1, 3, 3, 3, 3, 6, 8, 10, 11, 11}, 3, 8},
+	}
+
+	for _, c := range cases {
+		got := KthLargestUsingHeap(c.in, c.search)
 
 		if got != c.want {
 			t.Errorf("%vth largest number in %v == %v, want = %v", c.search, c.in, got, c.want)
